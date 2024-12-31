c3cba14945c7a08cf622bc9a17319f6920228056
Ferran <ferrangv98@gmail.com>
2024-12-31 23:06:43 +0100

    app.py fixes

--- app/models.py
+++
@@ -1,7 +0,0 @@
-from . import db
-
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(80), nullable=False, unique=True)
-    email = db.Column(db.String(120), nullable=False, unique=True)
-    password = db.Column(db.String(200), nullable=False)
